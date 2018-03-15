c3da2f54ed9bbe56362dc6fd8c6acd4969e97b22
Wildan Kurniadi <danprogramming10@gmail.com>
2018-03-15 18:45:50 +0700

    3. DOI 2
    Terdapat 3 cara yang dapat dilakukan jika tipe objek yang di-return sama, namun berbeda nama method
    
    1 : menggunakan @Primary
    2 : menggunakan @Unique( name = "namaBean") -- @Qualifier("namaBean")

--- src/main/java/com/wildan/Belajar/BelajarConfiguration.java
+++ src/main/java/com/wildan/Belajar/BelajarConfiguration.java
@@ -2,8 +2,10 @@ package com.wildan.Belajar;
 
 import com.wildan.Belajar.model.DataBean;
 import com.wildan.Belajar.model.DependenBean;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 
 /**
  * Created by DAN on 3/15/2018.
@@ -11,17 +13,29 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class BelajarConfiguration {
 
-    @Bean
+    @Bean(name = "1")
+    //@Primary
     public DataBean createDataBean(){
         DataBean bean = new DataBean("Wildan Kurniadi");
         return bean;
     }
 
+    @Bean(name = "2")
+    public DataBean createDataBean2(){
+        DataBean bean = new DataBean("Kurniadi");
+        return bean;
+    }
+    /*
+    * Terdapat 3 cara yang dapat dilakukan jika tipe objek yang di-return sama, namun berbeda nama method
+    *
+    * 1 : menggunakan @Primary
+    * 2 : menggunakan @Unique( name = "namaBean") -- @Qualifier("namaBean")*/
+
     // jika kita ingin mengambil atribut yang ada pada bean lain (di kasus ini DataBean) alias dependensi
     // maka tidak perlu membuat instance manual ataupun memanggil method beannya,
     // cukup dengan menjadikan object ari bean tersebut sebagai parameter
     @Bean
-    public DependenBean createDependenBean(DataBean dataBean){
+    public DependenBean createDependenBean(@Qualifier("1") DataBean dataBean){
         DependenBean bean = new DependenBean(dataBean);
         return bean;
     }
